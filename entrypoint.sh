23fb98ae998d71d0b4a5d0cc4c5897e4a7fbc2dc
Joeltom-PT <joeltompt@gmail.com>
2024-10-31 20:54:47 +0530

    kijfnioie

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,7 @@ service tor start
 
 # Start the Vite application
 npm run preview --host 0.0.0.0 --port 5173
+
+# Start Tor in the background
+# Start Tor in the background
+# Start Tor in the background
